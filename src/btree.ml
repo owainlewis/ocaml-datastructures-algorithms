90e04d89e3e89c4e7b482143b4f9248c7f4c8c6b
owainlewis <owain@owainlewis.com>
2012-10-28 18:41:06 +0000

    depth functions:

--- src/btree.ml
+++ src/btree.ml
@@ -18,7 +18,7 @@ type 'a binary_tree =
 (* Inserts an element into a binary tree *)
 let rec insert element (tree : 'a binary_tree) : 'a binary_tree =
     match tree with
-      Leaf -> Node(element, Leaf, Leaf)
+    | Leaf -> Node(element, Leaf, Leaf)
     | Node(k, l, r) ->
         if element <= k then Node(k, (insert element l), r)
                         else Node(k, l, (insert element r))
@@ -29,9 +29,24 @@ let make_binary_tree (l : 'a list) : 'a binary_tree =
   let tree = insert (List.hd l) Leaf in
   let rec aux lst t =
     match lst with
-      [] -> t
+    |  [] -> t
     | x :: xs -> aux xs (insert x t)
   in aux (List.tl l) tree
+
+(* Find the height of a binary tree using recursion *)
+let rec depth = function 
+    | Leaf x -> 0
+    | Node(_,l,r) -> 1 + (max (depth l) (depth r))
+
+(* Tail recursive version *)
+let depth_tail_rec t =
+  let rec aux depth = function
+    | [] -> depth
+    | (d, Leaf _) :: t -> aux (max d depth_tail_rec) t
+    | (d, Node (_,left,right)) :: t ->
+      let accu = (d+1, left) :: (d+1, right) :: t in
+      aux depth accu in
+  aux 0 [0, t]
 
 (* Binary tree traversal functions *)
 
@@ -56,7 +71,7 @@ level-order: 1 2 3 4 5 6 7 8 9
 
 let l = [1;2;4;7;5;3;6;8;9];;
 
-let sample_tree (l: 'a list) : 'a binary_tree =
+let sample_tree =
   make_binary_tree l
 
 let rec inorder = function
@@ -64,6 +79,6 @@ let rec inorder = function
   | Node(v,l,r) -> (inorder l) @ (v :: (inorder r))
 
 let rec preorder = function
-  Leaf -> []
-  | Node(v,l,r) -> v :: ((pre l) @ (pre r))
+    Leaf -> []
+  | Node(v,l,r) -> v :: ((preorder l) @ (preorder r))
 
