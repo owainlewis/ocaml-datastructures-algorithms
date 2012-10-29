4f1027cd584fb8f7395ec6e69e0234f7fd327574
owainlewis <owain@owainlewis.com>
2012-10-29 08:21:52 +0000

    move python code into separate folder

---
+++ src/python/bst.py
@@ -0,0 +1,24 @@
+# Binary Search Tree
+
+class TreeNode:
+
+  def __init__(self, payload, left=None, right=None):
+      self.payload = payload
+      self.left    = left
+      self.right   = right
+
+class BTree:
+
+  def __init__(self):
+      pass
+
+  def add: pass
+
+  def delete: pass
+
+main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
--- src/python/graphy.py
+++ src/python/graphy.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 # Directed Graph Algorithms 
 
 class Vertex:
