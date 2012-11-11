885c655430d9d298d6cf9b904c14d7feb03fa33c
owain lewis <owain@owainlewis.com>
2012-11-11 17:14:37 +0000

    playing with graphs

---
+++ src/math.ml
@@ -0,0 +1,6 @@
+(* Matrix transposition *)
+
+let a = Array2.of_array int 
+  [| 1; 2; 3; 4 |];
+  [| 5; 6; 7; 8 |];
+|]
--- src/python/adjlist.py
+++ src/python/adjlist.py
@@ -8,12 +8,38 @@ G = {'A': ['C', 'D'],
      'D': ['E'], 
      'E': [] }
 
+# Python implementation of the graph G above
+
+class AdjacencyGraph(object):
+
+    """ A class that represents a graph as an adjacency list """
+
+    def __init__(self):
+        self.node = {}
+        self.edges = {}
+
+    def add_node(self, node, **attrs):
+        if node not in self.edges:
+            self.edges[node] = {}
+            self.node[node] = attrs
+        else:
+            self.node[node].update(attrs)
+
+    def add_edge(self, u, v, **attrs): pass
+
+def make_graph():
+    gr = AdjacencyGraph()
+    items = ['A', 'B', 'C', 'D', 'E']
+    for v in items:
+        gr.add_node(v)
+    return gr
+
+gr = make_graph()
+
 class AdjList:
 
     def __init__(self, g):
         self.g = g
-
-    def __str__(self): pass
    
     def vertices(self):
         return self.g.keys()
