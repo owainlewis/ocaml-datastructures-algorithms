35997267d1282ce832f00608b4797dd1428a6cac
owainlewis <owain@owainlewis.com>
2012-11-13 14:13:02 +0000

    tidy up bst

--- src/python/bst.py
+++ src/python/bst.py
@@ -1,24 +1,55 @@
 # Binary Search Tree
 
-class TreeNode:
+class BinarySearchTree(object):
 
-  def __init__(self, payload, left=None, right=None):
-      self.payload = payload
-      self.left    = left
-      self.right   = right
+    def __init__(self, value=None):
+	self.left = None
+	self.right = None
+	self.value = value
 
-class BTree:
+    def insert(self, value):
+        """
+        Insert a value into the tree
+        """ 
+        # If value is empty
+        if self.value is None:
+            self.value = value
+        # Go left
+	elif self.value > value:
+	   if self.left is None:
+	      self.left = BinarySearchTree(value)
+	   else:
+	      self.left.insert(value)
+        # Go right
+	else:
+	   if self.right is None:
+	      self.right = BinarySearchTree(value)
+	   else:
+	      self.right.insert(value)
 
-  def __init__(self):
-      pass
-
-  def add: pass
-
-  def delete: pass
-
-main():
-    pass
+    def search(self, value, parent=None):
+        """
+            Search for a value in the tree
+        """
+        if value < self.value:
+            if self.left is None:
+                return False
+            return self.left.search(value, self)
+        elif value > self.value:
+            if self.right is None:
+                return False
+            return self.right.search(value, self)
+        else:
+            return True # value found
+            
+    def min_value(self, node): pass
+ 
+    def max_value(self, node): pass
 
 if __name__ == '__main__':
-    main()
-
+    b = BinarySearchTree()
+    b.insert(3)
+    b.insert(6)
+    b.insert(5)
+    b.insert(2)
+   
