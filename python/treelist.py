75eea6c37f7c96a6533f90668c7991afac72b0c2
owainlewis <owain@owainlewis.com>
2012-12-29 18:25:21 +0000

    organize folders

---
+++ python/treelist.py
@@ -0,0 +1,6 @@
+## Binary tree using list of lists approach
+
+def Tree(rootValue):
+    "Creates an empty tree"
+    return [rootValue, [], []]
+
---
+++ python/trie.py
@@ -0,0 +1,8 @@
+# 
+# 
+class TrieNode(object):
+
+    def __init__(self, word):
+        pass
+
+
