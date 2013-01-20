fe4238439e56e0c7cfa22e0df1272b198ab44d21
owain lewis <owain@owainlewis.com>
2013-01-20 19:49:52 +0000

    basic sorting stuff

--- src/sort/sort.ml
+++ src/sort/sort.ml
@@ -1,6 +1,17 @@
 (* Sorting algorithms *)
 
-let rec selection_sort l =
-  match l with
-    [] -> []
-    | x::xs -> []
+(* Utility function to do the hard work *)
+
+let rec insert item list =
+  match list with
+  | [] -> [item]
+  | x::xs -> 
+    match (item > x) with 
+    | true -> x::(insert item xs)
+    | false -> item::list
+
+let rec insertion_sort list = 
+  match list with
+  | [] -> [] (* Already sorted *)
+  | x::xs -> (insert x (insertionsort xs))
+
