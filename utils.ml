639165a67622dfe71d840e3775d26b58ce865036
owain lewis <owain@owainlewis.com>
2013-10-17 20:03:04 +0100

    utils

--- utils.ml
+++ utils.ml
@@ -1,5 +1,9 @@
 (* General utils *)
 
+module Util
+  struct
+  end
+
 let range (x: int) (y: int) =
   let rec aux l item =
     let increment n = n + 1
@@ -15,4 +19,16 @@ let rand_int (limit: int) : int =
 (* An int array of random size in range r *)
 let random_array size r =
   Array.init size (fun _ -> Random.int r)
+
+(* Useful operators *)
+
+let (|>) x f = f x 
+
+(* Haskells compose . operator *)
+let (>>) f g x = g (f x)
+
+let inc x = x + 1
+let dec x = x - 1
+
+let flat_map f = List.concat >> List.map f
 
