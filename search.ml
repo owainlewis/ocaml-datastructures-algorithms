e006126f67151a60e719c37dca6050ef40147ea0
owain lewis <owain@owainlewis.com>
2013-10-21 20:23:28 +0100

    easy stuff

---
+++ search.ml
@@ -0,0 +1,13 @@
+(* Search *)
+
+open Utils
+
+let r = Utils.random_list 100 100
+
+(* Nice easy one to start with *)
+let rec linear_search v = function
+  | [] -> None
+  | x::xs -> if x = v then Some(x) 
+                      else linear_search value xs
+
+
