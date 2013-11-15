2bc11c02e03dc0429864144647a92c46458a2058
Owain Lewis <owain@owainlewis.com>
2013-11-15 10:49:11 +0000

    Update search.ml

--- search.ml
+++ search.ml
@@ -10,7 +10,5 @@ module Search : SEARCHALG = struct
   (* Nice easy one to start with *)
   let rec linear v = function
     | [] -> None
-    | x::xs -> if x = v then Some(x) 
-                        else linear v xs
+    | x::xs -> if x = v then Some(x) else linear v xs
 end
-
