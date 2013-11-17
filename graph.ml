6d9a8f3f144057cd60ea2f9024ce0fa4a7933359
owain lewis <owain@owainlewis.com>
2013-11-17 12:04:42 +0000

    some logic for graphs

--- graph.ml
+++ graph.ml
@@ -2,6 +2,7 @@
 
 module type ADJ = 
   sig
+    type t
     (* A graph represented as a mutable list of vertices *)
     type graph
     (* A graph vertex in the form (v, incoming, outgoing) *)
@@ -9,28 +10,47 @@ module type ADJ =
     (* An edge in the form source -> dest -> weight *)
     type edge
     val create : unit -> graph
+    val vertices : graph -> int list
     val is_empty : graph -> bool
-    (* Adds a new vertex to a graph and returns the graph with an added vertex *)
     val add_vertex : graph -> int -> graph
+    val find_vertex : graph -> int -> vertex option
   end
 
-module Graph : ADJ = struct
+module Graph = struct
+
+  type t = int
+
   type vertex = V of int * ((vertex * int) list ref) * ((vertex * int) list ref) 
-  type edge = vertex * vertex * int
-  type graph = vertex list ref
+  type edge   = vertex * vertex * int
+  type graph  = vertex list ref
 
   let create () = ref []
 
+  let vertices g = 
+    List.map (fun (v) -> let V (x,_,_) = v in x) !g
+
+  (* TODO Duplication of logic *)
+
+  let out_func v = let V (_,x,_) = v in !x
+  let in_func v  = let V (_,_,x) = v in !x
+
+  let flatten_edges g f = 
+    let edges = List.map f !g in List.flatten edges
+
+  let outgoing_edges g = flatten_edges g out_func
+
+  let incoming_edges g = List.map (fun (v) -> let V (_,_,x) = v in x) !g
+ 
   let is_empty g =
     match !g with
     | [] -> true
     | _  -> false
 
-  let vertex_find graph vertex = 
-    let rec aux find g v =
-      match g with 
-      [] -> None
-      | x::xs -> if v = x then Some(x) else find xs v
+  let find_vertex graph vertex =
+    let rec find g v =
+      match g with
+      | [] -> None
+      | V (x,_,_) as vtx :: xs -> if v = x then Some(vtx) else find xs v
     in find !graph vertex
 
   let add_vertex graph v = 
