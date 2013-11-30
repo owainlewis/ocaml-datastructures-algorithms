dbb844935e0cfc98dda4cbdc7af2f2aa94240e1a
owain lewis <owain@owainlewis.com>
2013-11-30 23:22:45 +0000

    more exercises

--- avl_tree.py
+++ avl_tree.py
@@ -1,6 +1,11 @@
 # AVL Trees
 
+from __future__ import print_function
+
 class BSTNode(object):
+
+    """ A simple BST node object """
+
     def __init__(self, t):
         self.key    = t
         self.dissoc()
@@ -10,6 +15,9 @@ class BSTNode(object):
         self.right = None
         self.parent = None
 
+    def is_leaf(self):
+        return self.key is None
+
     def __repr__(self): 
         return "Node: < %s >" % self.key
 
@@ -18,8 +26,12 @@ class BSTNode(object):
 
 class BST(object):
 
-    def __init__(self):
+    """ A basic Binary Search Tree """
+
+    def __init__(self, values=[]):
         self.root = None
+        if any(values):
+            self.insert_many(values)
 
     def insert(self, k):
         """Insert a key k into the tree"""
@@ -75,7 +87,48 @@ class BST(object):
             parent = node.parent
             node.dissoc()
             return node, parent
-   
+
+    # This is easier in OCaml
+    def height(self, node):
+        if node is None:
+            return 0
+        else:
+            return 1 + max(self.height(node.left), self.height(node.right))
+
+    # Traversals 
+
+    def print_func (self, x): 
+        print(x) # wrong version of python to pass as a lambda
+
+    def inorder_traversal(self, f, node):
+        if node is not None:          
+            self.inorder_traversal(f, node.left)
+            f(node.key)
+            self.inorder_traversal(f, node.right)
+
+    def print_tree_in_order(self):
+        self.inorder_traversal(self.print_func, self.root)
+
+    def preorder_traversal(self, f, node):
+        if node is not None:
+            f(node.key)
+            self.preorder_traversal(f, node.left)
+            self.preorder_traversal(f, node.right)
+
+    def print_tree_in_preorder(self):
+        self.preorder_traversal(self.print_func, self.root)
+
+    def postorder_traversal(self, f, node):
+        if node is not None:
+            self.postorder_traversal(f, node.left)
+            self.postorder_traversal(f, node.right)
+            f(node.key)
+
+    def print_tree_in_postorder(self):
+        self.postorder_traversal(self.print_func, self.root)
+        
+test = [3, 5, 6, 7, 10, 12, 13, 15, 16, 18, 20, 23]
+
 # AVL Tree
 ##########################################
 
