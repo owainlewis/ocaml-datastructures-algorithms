924ecfc1b5a5d78dd071480c1a822a1f4e5e5a1f
owain lewis <owain@owainlewis.com>
2014-02-02 16:55:54 +0000

    distance metrics

--- distance/distance.ml
+++ distance/distance.ml
@@ -1,10 +1,30 @@
 (* OCaml distance metrics *)
 
-module Distance = struct
+class point_type x_pos y_pos = object
+  val mutable x: float = x_pos
+  val mutable y: float = y_pos
+  method x = x
+  method y = y
 end
 
-let chebyshev () = ()
+module Distance = struct
+  type point = Point of float * float
 
-let euclidean () = ()
+  let as_tuple p = match p with Point(x,y) -> (x,y)
+  (* This is also known as chessboard distance *)
+  let chebyshev p1 p2 =
+    let (x1, y1) = as_tuple p1
+    and (x2, y2) = as_tuple p2 in
+    let a = abs_float (x1 -. x2)
+    and b = abs_float (y1 -. y2) in
+    max a b
 
-let manhattan () = ()
+  let euclidean () = ()
+
+  let manhattan () = ()
+end
+
+let test x1 y1 x2 y2 =
+  let p1 = Distance.Point(x1,y1)
+  and p2 = Distance.Point(x2,y2) in
+  Distance.chebyshev p1 p2
