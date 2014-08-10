ebe922b41fad64ef41d4369ec6bd0ae47352ef5a
Owain Lewis <owain@owainlewis.com>
2014-08-10 16:44:04 +0100

    Update distance.ml

--- distance/distance.ml
+++ distance/distance.ml
@@ -1,4 +1,5 @@
 (* OCaml distance metrics *)
+
 class point_type x_pos y_pos = object
   val mutable x: float = x_pos
   val mutable y: float = y_pos
@@ -7,19 +8,25 @@ class point_type x_pos y_pos = object
 end
 
 module Distance = struct
+
   type point = Point of float * float
+  
   let as_tuple p = match p with Point(x,y) -> (x,y)
+  
   let px point = match point with
     | Point(x, _) -> Some(x)
     | _ -> None
+    
   let py point = match point with
     | Point(_, y) -> Some(y)
     | _ -> None
+    
   (* This is also known as chessboard distance *)
   let chebyshev (x1,y1) (x2,y2) =
     let a = abs_float (x1 -. x2)
     and b = abs_float (y1 -. y2) in
     max a b
+    
  (* The euclidean distance for a two points is simply
     euclidean distance = ((x, y), (a, b)) = sqrt (x - a)2 + (y - b)2 *)
   let euclidean (x1,y1) (x2,y2) =
