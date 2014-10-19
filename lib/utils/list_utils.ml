4e3dad8ad24a3f56f1b05eb529b36edeaad9bf57
Owain Lewis <owain@owainlewis.com>
2014-10-19 21:23:26 +0100

    Update list_utils.ml

--- lib/utils/list_utils.ml
+++ lib/utils/list_utils.ml
@@ -20,7 +20,8 @@ let rec replicate n xs =
   let (|>) v f = f v in
   let rec aux n = function
     | [] -> []
-    | x::xs -> let y = repeat x n in y :: replicate n xs
+    | x::xs -> let y = repeat x n 
+               in y :: replicate n xs
   in let result = aux n xs
   in result |> List.fold_left (fun a b -> b :: a) []
 end
