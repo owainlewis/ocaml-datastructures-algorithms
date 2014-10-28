b56167c43496305bc44f6d1b56518e234a4173b7
Owain Lewis <owain@owainlewis.com>
2014-10-28 14:30:08 +0000

    merge

--- src/algorithms/sorts/merge.ml
+++ src/algorithms/sorts/merge.ml
@@ -1,1 +1,25 @@
 (* Merge sort *)
+
+module MergeSort = struct
+
+let rec split_at n xs =
+  match n, xs with
+    0, xs -> [], xs
+  | n, x::xs when n > 0 ->
+    let y, z = split_at (n-1) xs in
+      x::y, z
+  | _, _ -> failwith "Negative index"
+
+(* Divide a list into two parts *)
+let divide xs = split_at (List.length xs / 2) xs
+
+let rec sort cmp = function
+    [] -> []
+  | [x] -> [x]
+  | xs ->
+      let xs, ys = divide xs in
+      List.merge cmp (merge_sort cmp xs) (merge_sort cmp ys)
+
+end
+
+let eg = MergeSort.sort compare [8;6;4;2;1;3;5;7;9]
