d3a27cf8f2ef96e21998be4f553b71191d17dec4
Owain Lewis <owain.lewis@oracle.com>
2019-09-18 20:58:47 +0100

    Run ocp-indent

--- lib/bst.ml
+++ lib/bst.ml
@@ -1,29 +1,29 @@
 type 'a tree =
-    | Leaf
-    | Node of 'a tree * 'a * 'a tree
-                
+  | Leaf
+  | Node of 'a tree * 'a * 'a tree
+
 let empty = Leaf
 
 let rec insert v t = match t with
-    | Leaf -> Node(Leaf, v, Leaf)
-    | Node(l,x,r) ->
-        if v <= x then
-          Node((insert v l), x, r)
-        else
-          Node(l, x, (insert v r))
+  | Leaf -> Node(Leaf, v, Leaf)
+  | Node(l,x,r) ->
+    if v <= x then
+      Node((insert v l), x, r)
+    else
+      Node(l, x, (insert v r))
 
-  let rec member v = function
-    | Leaf -> false
-    | Node(l,x,r) ->
-        if v = x then true
-        else
-          if v < x then member v l
-          else member v r
+let rec member v = function
+  | Leaf -> false
+  | Node(l,x,r) ->
+    if v = x then true
+    else
+    if v < x then member v l
+    else member v r
 
-  let rec height = function
-    | Leaf -> 1
-    | Node(l,_,r) -> max (1 + height l) (1 + height r)
+let rec height = function
+  | Leaf -> 1
+  | Node(l,_,r) -> max (1 + height l) (1 + height r)
 
-  let rec tree_fold f acc = function
-    | Leaf -> acc
-    | Node(l,x,r) -> f x (tree_fold f acc l) (tree_fold f acc r)
+let rec tree_fold f acc = function
+  | Leaf -> acc
+  | Node(l,x,r) -> f x (tree_fold f acc l) (tree_fold f acc r)
