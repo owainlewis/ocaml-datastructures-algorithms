e1d7cdd04b4e243bb7447c8a885824dced5b0c5d
Owain Lewis <owain.lewis@oracle.com>
2019-09-21 14:42:49 +0100

    Binary tree deletion algorithm

--- lib/bst.ml
+++ lib/bst.ml
@@ -1,26 +1,58 @@
+exception Unknown
+
 type 'a tree =
   | Leaf
   | Node of 'a tree * 'a * 'a tree
 
-let empty = Leaf
-
-let rec insert v t = match t with
-  | Leaf -> Node(Leaf, v, Leaf)
-  | Node(l,x,r) as t ->
-    if v < x then Node((insert v l), x, r)
-    else if v > x then Node(l, x, (insert v r))
-    (* Do nothing, duplicate key *)
-    else t 
-
-(** TODO delete a node from the tree **)
-let delete t = t
+let leaf = Leaf
   
-let rec min t = match t with
-  | Leaf -> None
-  | Node(l,x,_) ->
-      if l = Leaf then Some(x) else min l
+let node x = Node(leaf, x, leaf)
 
-let rec member v = function
+let rec min_value tree =
+  match tree with
+    | Leaf -> raise Unknown
+    | Node(l,x,_) ->
+      if l = Leaf then x else min_value l
+
+let rec max_value tree =
+  match tree with
+  | Leaf -> raise Unknown
+  | Node(_,x,r) ->
+    if r = Leaf then x else max_value r
+        
+let rec insert value tree =
+  match tree with
+    | Leaf -> Node(Leaf, value, Leaf)
+    | Node(l,x,r) as t ->
+      if value < x then Node((insert value l), x, r)
+      else if value > x then Node(l, x, (insert value r))
+      (* Do nothing, duplicate key *)
+      else t 
+
+(* Deletion in a binary search tree has three cases
+ * 1.) node to be deleted is a leaf
+ * 2.) node to be deleted has only one child
+ * 3.) node to be deleted has two children
+ *)
+let rec delete value tree =
+  match tree with
+  | Leaf -> tree
+  | Node(l,x,r) ->
+    if value = x then
+      (* node to be deleted is a leaf *)
+      if l = Leaf && r = Leaf then Leaf
+      (* node to be deleted has only one child *)
+      else if l = Leaf then r
+      else if r = Leaf then l        
+      else let m = min_value l in
+           let n = delete m r in Node(l,m,n) 
+    else if value < x then 
+      let lhs = delete value l in Node(lhs,x,r)
+    else
+      let rhs = delete value r in Node(l,x,rhs)
+    
+let rec member v tree =
+  match tree with
   | Leaf -> false
   | Node(l,x,r) ->
     if v = x then true
@@ -37,5 +69,6 @@ let rec fold f acc = function
   | Leaf -> acc
   | Node(l,x,r) -> f x (fold f acc l) (fold f acc r)
 
-(** Examples **)
-let example = insert 5 Leaf |> insert 4 |> insert 3
+(** Construct a new tree from a list of values **)
+let build xs = List.fold_left (fun x y -> insert y x) Leaf xs
+
