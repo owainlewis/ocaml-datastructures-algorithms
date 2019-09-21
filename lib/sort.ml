4cc1aa0c2e10d433e7c741df4337915379d2f536
Owain Lewis <owain.lewis@oracle.com>
2019-09-21 14:54:17 +0100

    Update algo

--- lib/sort.ml
+++ lib/sort.ml
@@ -1,13 +1,11 @@
 (** Sorting Algorithms **)
 
-let rec bubble_sort xs =
-  let rec switch_all = function
+let rec bubble xs =
+  let rec switch_all ys = match ys with
     | []    -> []
     | x::[] -> [x]
     | x::y::xs when x > y -> y::switch_all(x::xs)
     | x::y::xs -> x :: switch_all(y::xs)
   in
-  let v = switch_all xs in
-  (* if nothing has changed it's sorted otherwise do another sweep *)
-  if v = xs then v
-  else bubble_sort v
+  let sweep = switch_all xs in
+  if sweep = xs then sweep else bubble sweep
