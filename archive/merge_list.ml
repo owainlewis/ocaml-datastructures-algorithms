501a35064d2b01544269c700c3a05fdb3e3d7f57
annemariepeek <annemariepeek@hotmail.com>
2022-11-30 15:05:54 -0500

    add merge_list.ml

---
+++ archive/merge_list.ml
@@ -0,0 +1,9 @@
+(* Merge two sorted lists, lst1 and lst2, and return the result as a sorted list *)
+
+let rec merge_list l1 l2 =
+  match l1 with 
+  | [] -> l2
+  | (a::b) -> (match l2 with
+      | [] -> l1
+      | (h::t) when a <= h -> a::(merge_list l2 b)
+      | (h::t) -> h::(merge_list l1 t))
